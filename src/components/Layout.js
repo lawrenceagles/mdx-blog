276f2fa7631aeed75073fa112dc2a8add481bb3a
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-18 10:55:16 +0100

    navbar done

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,17 +5,19 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 const Layout = ({ children }) => {
-  return (
-    <>
-      <h2>layout component</h2>
-      <main>{children}</main>
-    </>
-  )
-}
+	return (
+		<React.Fragment>
+			<Navbar />
+			<Sidebar />
+			<main>{children}</main>
+			<Footer />
+		</React.Fragment>
+	);
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Layout from '../components/Layout'
-import Posts from '../components/Posts'
-import { graphql } from 'gatsby'
-import SEO from '../components/SEO'
+import React from "react";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
+import Posts from "../components/Posts";
+import { graphql } from "gatsby";
+import SEO from "../components/SEO";
 // ...GatsbyImageSharpFluid
 const IndexPage = () => {
-  return <h2>home page</h2>
-}
+	return <Layout>Home page</Layout>;
+};
 
-export default IndexPage
+export default IndexPage;
