52e2941332e7be1cc76115326d12e00b9af84ecf
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-18 15:38:46 +0100

    added success page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Links from "../constants/links";
 import Categories from "../components/Categories";
 import { IoMdClose } from "react-icons/io";
+
 const Sidebar = ({ isOpen, toggle }) => {
 	return (
 		<aside className={`sidebar ${isOpen && "showSidebar"}`}>
