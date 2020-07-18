c008d4bfb215f6b248f88f65b075b6fdaf25869b
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-18 11:24:16 +0100

    added sidebar toggle

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,15 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
+
 const Layout = ({ children }) => {
+	const [ isOpen, setIsOpen ] = useState(false);
+	const toggle = () => setIsOpen(!isOpen);
 	return (
 		<React.Fragment>
-			<Navbar />
-			<Sidebar />
+			<Navbar toggle={toggle} />
+			<Sidebar isOpen={isOpen} toggle={toggle} />
 			<main>{children}</main>
 			<Footer />
 		</React.Fragment>
