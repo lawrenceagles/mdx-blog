0a30cc92e93bbe3867bda484b6d60bae84ad6366
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-18 15:35:40 +0100

    finished netlify form integration

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -7,7 +7,16 @@ const NewsLetter = () => {
 				<div className='page-center'>
 					<h2>Get all the latest stories to your inbox</h2>
 					<h4>I write to my friends every few weeks</h4>
-					<form className='contact-form' netlify name='contact' method='post' netlify-honeypot='bot-field'>
+					<form
+						className='contact-form'
+						netlify
+						name='contact'
+						method='post'
+						netlify-honeypot='bot-field'
+						action='/success'
+					>
+						<input type='hidden' name='bot-field' />
+						<input type='hidden' name='form-name' value='contact' />
 						<input type='text' name='name' placeholder='Your name' className='form-control' />
 						<input type='email' name='email' placeholder='Your emal' className='form-control' />
 						<button type='submit' className='btn form-control submit-btn'>
