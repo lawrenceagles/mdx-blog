3b14a89ef21be151b564833eaa8b4becb8feefb7
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-19 22:29:42 +0100

    added category page

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -1,11 +1,46 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import Posts from '../components/Posts'
-import { graphql } from 'gatsby'
+import React from "react";
+import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+import Posts from "../components/Posts";
+import { graphql } from "gatsby";
 
-const CategoryTemplate = () => {
-  return <h2>categories template</h2>
-}
+const CategoryTemplate = (props) => {
+	const { pageContext: { category } } = props;
+	const { data: { categories: { nodes: posts } } } = props;
+	return (
+		<Layout>
+			<Hero />
+			<Posts posts={posts} title={`category/${category}`} />
+		</Layout>
+	);
+};
 
-export default CategoryTemplate
+export const query = graphql`
+	query GetCategory($category: String) {
+		categories: allMdx(
+			sort: { fields: frontmatter___date, order: DESC }
+			filter: { frontmatter: { category: { eq: $category } } }
+		) {
+			nodes {
+				frontmatter {
+					slug
+					title
+					category
+					date(formatString: "MMMM, Do, YYYY")
+					readTime
+					author
+					image {
+						childImageSharp {
+							fluid {
+								...GatsbyImageSharpFluid
+							}
+						}
+					}
+				}
+				excerpt
+			}
+		}
+	}
+`;
+
+export default CategoryTemplate;
