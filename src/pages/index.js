c094331d3482958c7d43361d8ba4d8059bf0d611
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-19 20:36:38 +0100

    added post

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,41 @@ import Layout from "../components/Layout";
 import Posts from "../components/Posts";
 import { graphql } from "gatsby";
 import SEO from "../components/SEO";
-// ...GatsbyImageSharpFluid
-const IndexPage = () => {
+
+const IndexPage = ({ data }) => {
+	const { allMdx: { nodes: posts } } = data;
 	return (
 		<Layout>
 			<Hero showPerson />
+			<Posts posts={posts} title='recently published' />
 		</Layout>
 	);
 };
 
 export default IndexPage;
+
+export const query = graphql`
+	{
+		allMdx(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+			nodes {
+				excerpt
+				frontmatter {
+					title
+					author
+					category
+					date(formatString: "MM Do, YYYY")
+					slug
+					readTime
+					image {
+						childImageSharp {
+							fluid {
+								...GatsbyImageSharpFluid
+							}
+							id
+						}
+					}
+				}
+			}
+		}
+	}
+`;
