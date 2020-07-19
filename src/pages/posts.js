d3baf397939f33f0da5af0a205d1d8c1cea3d4b5
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-19 20:43:40 +0100

    posts page added

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
-import { graphql } from 'gatsby'
-import Posts from '../components/Posts'
-// ...GatsbyImageSharpFluid
+import React from "react";
+import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+import { graphql } from "gatsby";
+import Posts from "../components/Posts";
 const PostsPage = ({ data }) => {
-  return <h2>posts page</h2>
-}
+	const { allMdx: { nodes: posts } } = data;
+	return (
+		<Layout>
+			<Hero />
+			<Posts posts={posts} title='all posts' />
+		</Layout>
+	);
+};
 
-export default PostsPage
+export const query = graphql`
+	{
+		allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+			nodes {
+				excerpt
+				frontmatter {
+					title
+					author
+					category
+					date(formatString: "MM Do, YYYY")
+					slug
+					readTime
+					image {
+						childImageSharp {
+							fluid {
+								...GatsbyImageSharpFluid
+							}
+							id
+						}
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default PostsPage;
