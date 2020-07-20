bf14e3e1a1797ac701f50ee5708239350cd8d415
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-20 15:07:26 +0100

    added about and title in banner

--- src/components/Banner/About.js
+++ src/components/Banner/About.js
@@ -1,22 +1,43 @@
-import React from 'react'
-import SocialLinks from '../../constants/socialLinks'
-import Image from 'gatsby-image'
-import { graphql, useStaticQuery } from 'gatsby'
-import Title from './Title'
-import styled from 'styled-components'
-//...GatsbyImageSharpFixed
+import React from "react";
+import SocialLinks from "../../constants/socialLinks";
+import Image from "gatsby-image";
+import { graphql, useStaticQuery } from "gatsby";
+import Title from "./Title";
+import styled from "styled-components";
+
+export const query = graphql`
+	{
+		file(relativePath: { eq: "banner-about.jpeg" }) {
+			childImageSharp {
+				fixed(width: 100, height: 100) {
+					...GatsbyImageSharpFixed
+				}
+			}
+		}
+	}
+`;
 
 const About = () => {
-  return <Wrapper>Banner About</Wrapper>
-}
+	const data = useStaticQuery(query);
+	return (
+		<Wrapper>
+			<Title title='about me' />
+			<Image fixed={data.file.childImageSharp.fixed} className='img' />
+			<p>
+				Adaptogen taiyaki before they sold out scenester godard you probably haven't heard of them chartreuse.
+			</p>
+			<SocialLinks styleClass='banner-icons' />
+		</Wrapper>
+	);
+};
 
 const Wrapper = styled.div`
-  text-align: center;
-  p {
-    color: var(--clr-grey-5);
-  }
-  .img {
-    border-radius: 50%;
-  }
-`
-export default About
+	text-align: center;
+	p {
+		color: var(--clr-grey-5);
+	}
+	.img {
+		border-radius: 50%;
+	}
+`;
+export default About;
