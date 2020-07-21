e97b125404c1d6925120e20e7fe8ff439ed4b978
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-21 22:34:35 +0100

    added social links

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,28 +1,24 @@
-import React from "react"
-import {
-  FaFacebookSquare,
-  FaDribbbleSquare,
-  FaTwitterSquare,
-} from "react-icons/fa"
+import React from "react";
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
 const SocialLinks = ({ styleClass }) => {
-  return (
-    <ul className={styleClass}>
-      <li>
-        <a href="https://twitter.com">
-          <FaFacebookSquare className="social-icon facebook-icon"></FaFacebookSquare>
-        </a>
-      </li>
-      <li>
-        <a href="https://twitter.com">
-          <FaDribbbleSquare className="social-icon dribble-icon"></FaDribbbleSquare>
-        </a>
-      </li>
-      <li>
-        <a href="https://twitter.com">
-          <FaTwitterSquare className="social-icon twitter-icon"></FaTwitterSquare>
-        </a>
-      </li>
-    </ul>
-  )
-}
-export default SocialLinks
+	return (
+		<ul className={styleClass}>
+			<li>
+				<a href='https://www.facebook.com/lawrence.oputa.1029'>
+					<FaFacebookSquare className='social-icon facebook-icon' />
+				</a>
+			</li>
+			<li>
+				<a href='https://instagram.com/lawrence_eagles'>
+					<FaInstagramSquare className='social-icon dribble-icon' />
+				</a>
+			</li>
+			<li>
+				<a href='https://twitter.com/lawrence_eagles'>
+					<FaTwitterSquare className='social-icon twitter-icon' />
+				</a>
+			</li>
+		</ul>
+	);
+};
+export default SocialLinks;
