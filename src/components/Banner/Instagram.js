f545b52463f53ff3b17736eb9fbb5a57c70cfe0e
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-21 19:43:22 +0100

    added instagram feeds

--- src/components/Banner/Instagram.js
+++ src/components/Banner/Instagram.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import Title from './Title'
-import Image from 'gatsby-image'
-import styled from 'styled-components'
-import { graphql, useStaticQuery } from 'gatsby'
-//...GatsbyImageSharpFluid
+import React from "react";
+import Title from "./Title";
+import Image from "gatsby-image";
+import styled from "styled-components";
+import { graphql, useStaticQuery } from "gatsby";
+
+const query = graphql`
+	{
+		allInstaNode(limit: 6) {
+			nodes {
+				localFile {
+					childImageSharp {
+						fluid {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		}
+	}
+`;
 
 const Instagram = () => {
-  return <Wrapper>Banner Instagram</Wrapper>
-}
+	const data = useStaticQuery(query);
+	const { allInstaNode: { nodes } } = data;
+	return (
+		<Wrapper>
+			<Title title='instagram' />
+			<div className='images'>
+				{nodes.map((item, index) => {
+					const { localFile: { childImageSharp: { fluid } } } = item;
+					return <Image fluid={fluid} key={index} />;
+				})}
+			</div>
+		</Wrapper>
+	);
+};
 
 const Wrapper = styled.article`
-  .images {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 1rem;
-  }
-`
+	.images {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		gap: 1rem;
+	}
+`;
 
-export default Instagram
+export default Instagram;
