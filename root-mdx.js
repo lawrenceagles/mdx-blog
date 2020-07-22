10031ebd14a884f38d3e6fe1bd1b4607cfd5bb56
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-22 20:48:37 +0100

    added prism renderer code-block styling

--- root-mdx.js
+++ root-mdx.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
+import { Headings, Code, Blockquote, Example } from "./src/components/Complete";
 
-const components = {};
+const components = {
+	h2         : Headings.myH2,
+	h4         : Headings.myH4,
+	inlineCode : Code,
+	blockquote : Blockquote,
+	pre        : Example,
+};
 
 export const GlobalMDXWrapper = ({ element }) => {
 	return <MDXProvider components={components}>{element}</MDXProvider>;
